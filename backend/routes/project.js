d9186e550dfe19f7972282d775b9277f91615b04
Andrius Bogda <AndriusBogda@coherentsolutions.com>
2023-03-12 14:04:09 +0200

    added project stuff

---
+++ backend/routes/project.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const controller = require("../controllers/project");
+
+const router = express.Router();
+// /profile/create-project
+router.post("/create-project", controller.Addproject);
+// /profile
+router.get("/", controller.project);
+
+module.exports = router;
