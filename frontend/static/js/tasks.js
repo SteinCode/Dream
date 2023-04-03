281e90b319bfabc1451f3cba497929ddccbbb9d4
Ignas Dūdėnas <dudenasignas@gmail.com>
2023-04-03 19:40:12 +0300

    Tasks code refactored unnecessary comments deleted

--- frontend/static/js/tasks.js
+++ frontend/static/js/tasks.js
@@ -27,119 +27,53 @@ workerRows.forEach((row) => {
   });
 });
 
-// // Task dragging logic
-// document.addEventListener("DOMContentLoaded", (event) => {
-//   var dragSrcEl = null;
+let cards = document.querySelectorAll(".task-card");
+let lists = document.querySelectorAll(".task-list");
 
-//   function handleDragStart(e) {
-//     this.style.outline = "3px dashed #c4cad3";
-
-//     dragSrcEl = this;
-
-//     e.dataTransfer.effectAllowed = "move";
-//     e.dataTransfer.setData("text/html", this.innerHTML);
-//   }
-
-//   function handleDragOver(e) {
-//     if (e.preventDefault) {
-//       e.preventDefault();
-//     }
-
-//     e.dataTransfer.dropEffect = "move";
-
-//     return false;
-//   }
-
-//   function handleDragEnter(e) {
-//     this.classList.add("task-hover");
-//   }
-
-//   function handleDragLeave(e) {
-//     this.classList.remove("task-hover");
-//   }
-
-//   function handleDrop(e) {
-//     if (e.stopPropagation) {
-//       e.stopPropagation(); // stops the browser from redirecting.
-//     }
-
-//     if (dragSrcEl != this) {
-//       dragSrcEl.innerHTML = this.innerHTML;
-//       this.innerHTML = e.dataTransfer.getData("text/html");
-//     }
-
-//     return false;
-//   }
-
-//   function handleDragEnd(e) {
-//     this.style.opacity = "1";
-//     this.style.border = 0;
-
-//     items.forEach(function (item) {
-//       item.classList.remove("task-hover");
-//     });
-//   }
-
-//   let items = document.querySelectorAll(".task");
-//   items.forEach(function (item) {
-//     item.addEventListener("dragstart", handleDragStart, false);
-//     item.addEventListener("dragenter", handleDragEnter, false);
-//     item.addEventListener("dragover", handleDragOver, false);
-//     item.addEventListener("dragleave", handleDragLeave, false);
-//     item.addEventListener("drop", handleDrop, false);
-//     item.addEventListener("dragend", handleDragEnd, false);
-//   });
-// });
-
-
-let cards = document.querySelectorAll('.task-card');
-let lists = document.querySelectorAll('.task-list');
-
-
-cards.forEach((card)=>{
-    registerEventsOnCard(card);
+cards.forEach((card) => {
+  registerEventsOnCard(card);
 });
 
-lists.forEach((list)=>{
-    list.addEventListener('dragover', (e)=>{
-        e.preventDefault();
-        let draggingCard = document.querySelector('.dragging');
-        let cardAfterDraggingCard = getCardAfterDraggingCard(list, e.clientY);
-        if(cardAfterDraggingCard){
-            
-                cardAfterDraggingCard.parentNode.insertBefore(draggingCard, cardAfterDraggingCard);
-        } else{
-            list.appendChild(draggingCard);
-        }
-        
-    });
+lists.forEach((list) => {
+  list.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    let draggingCard = document.querySelector(".dragging");
+    let cardAfterDraggingCard = getCardAfterDraggingCard(list, e.clientY);
+    if (cardAfterDraggingCard) {
+      cardAfterDraggingCard.parentNode.insertBefore(
+        draggingCard,
+        cardAfterDraggingCard
+      );
+    } else {
+      list.appendChild(draggingCard);
+    }
+  });
 });
 
-function getCardAfterDraggingCard(list, yDraggingCard){
+function getCardAfterDraggingCard(list, yDraggingCard) {
+  let listCards = [...list.querySelectorAll(".task-card:not(.dragging)")];
 
-    let listCards = [...list.querySelectorAll('.task-card:not(.dragging)')];
+  return listCards.reduce(
+    (closestCard, nextCard) => {
+      let nextCardRect = nextCard.getBoundingClientRect();
+      let offset = yDraggingCard - nextCardRect.top - nextCardRect.height / 2;
 
-    return listCards.reduce((closestCard, nextCard)=>{
-        let nextCardRect = nextCard.getBoundingClientRect();
-        let offset = yDraggingCard - nextCardRect.top - nextCardRect.height /2;
-
-        if(offset < 0 && offset > closestCard.offset){
-            return {offset, element: nextCard}
-        } else{
-            return closestCard;
-        }
-    
-    }, {offset: Number.NEGATIVE_INFINITY}).element;
-
+      if (offset < 0 && offset > closestCard.offset) {
+        return { offset, element: nextCard };
+      } else {
+        return closestCard;
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element;
 }
 
-function registerEventsOnCard(card){
-    card.addEventListener('dragstart', (e)=>{
-        card.classList.add('dragging');
-    });
+function registerEventsOnCard(card) {
+  card.addEventListener("dragstart", (e) => {
+    card.classList.add("dragging");
+  });
 
-
-    card.addEventListener('dragend', (e)=>{
-        card.classList.remove('dragging');
-    });
+  card.addEventListener("dragend", (e) => {
+    card.classList.remove("dragging");
+  });
 }
