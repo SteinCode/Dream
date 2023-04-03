93fa11f13a493a20acf0a66adad54549c0f2b1a1
Andrius Bogda <AndriusBogda@coherentsolutions.com>
2023-04-03 21:06:57 +0300

    added unit test for inserting project to db

--- tests/unit/project_page.test.js
+++ tests/unit/project_page.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const controller = require("../controllers/project");
 const { app, db } = require('../../server.js'); // assuming db and app are exported in server.js
 const expect = require('chai').expect;
 const bodyParser = require('body-parser');
@@ -14,28 +15,39 @@ describe('Project routes', () => {
         console.log(error);
       } else {
         console.log("Database connected!");
+        server = app.listen(3000, () => {
+          console.log("Server started on port 3000!");
+          done();
+        });
       }
-      done();
     });
   });
 
   afterEach((done) => {
-    db.end(done);
+    server.close(() => {
+      console.log("Server closed!");
+      db.end(done);
+    });
   });
-  
 
   describe('POST /profile/create-project', () => {
     it('should return status 200', async () => {
-      const res = await request(app)
+      const res = await request(server)
         .post('/profile/create-project')
+        .send({ projectName: 'Test Project', users: [{ id: 1 }, { id: 2 }] })
+        .set('Cookie', ['token=xyz'])
         .expect(200);
+      expect(res.body).to.have.property('projectId');
     });
   });
 
   describe('GET /profile', () => {
     it('should return status 200', async () => {
-      const res = await request(app)
+      const res = await request(server)
         .get('/profile')
+        .set('Cookie', ['token=xyz'])
         .expect(200);
+      expect(res.text).to.contain('Welcome to your profile page!');
     });
   });
+});
