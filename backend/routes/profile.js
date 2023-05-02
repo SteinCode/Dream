50c7dedb659d2999de47e599a4b735bb31b2fd85
Rokas Reizgys <rokas.student@gmail.com>
2023-05-02 15:41:43 +0300

    Fixed profile page bugs

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -2,9 +2,8 @@ const express = require("express");
 const controller = require("../controllers/profile");
 
 const router = express.Router();
-// /profile/updateUser
-router.post("/", controller.updateUser);
 // /profile
 router.get("/", controller.profile);
-
+// /profile/updateUser
+router.post("/update-user", controller.updateUser);
 module.exports = router;
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -4,7 +4,6 @@ const controller = require("../controllers/tasks");
 const router = express.Router();
 // /profile
 router.get("/", controller.tasks);
-// router.get("/developers", controller.getDeveloperByProject); //this temporary returns all developers only without checking the project
 router.post("/create-task", controller.createTask);
 router.put("/update-task-status/:id", controller.updateTaskStatus);
 module.exports = router;
